e92042b23b2f075a3523e317e8a0b4642ddb7222
Daniel Zepeda <autohost+dz@racksace.com>
2011-05-30 10:14:51 -0500

    Remove growl notify, remove argument parsing.

--- plugin/sweet-vim-rspec.vim
+++ plugin/sweet-vim-rspec.vim
@@ -1,13 +1,7 @@
 
-function! SweetRunSpec(command)
-  " TODO: handle args such as --tag focus here, or make a separate command for it
-  if a:command == ''
-    let dir = 'spec'
-  else
-    let dir = a:command
-  endif
-  cexpr system("bundle exec rspec -r /Users/dzepeda/.vim/plugin/my_spec_formatter.rb -f RSpec::Core::Formatters::VimFormatter " . expand("%:p"). " -l " . line("."))
+function! SweetRunSpec()
+  cexpr system("bundle exec rspec -r /Users/dzepeda/.vim/ruby/sweet_vim_rspec_formatter.rb -f RSpec::Core::Formatters::VimFormatter " . expand("%:p"). " -l " . line("."))
   cw
 endfunction
-command! -nargs=* -complete=file SweetSpec call SweetRunSpec(<q-args>)
+command! -nargs=0 -complete=file SweetSpec call SweetRunSpec()
 map <leader>s :SweetSpec<space>
--- ruby/sweet_vim_rspec_formatter.rb
+++ ruby/sweet_vim_rspec_formatter.rb
@@ -45,13 +45,13 @@ module RSpec
         def close
           super
           summary = summary_line example_count, failure_count, pending_count
-          if failure_count > 0
-            growlnotify "--image ./autotest/fail.png -p Emergency -m '#{summary}' -t 'Spec failure detected'"
-          elsif pending_count > 0
-            growlnotify "--image ./autotest/pending.png -p High -m '#{summary}' -t 'Pending spec(s) present'"
-          else
-            growlnotify "--image ./autotest/pass.png -p 'Very Low' -m '#{summary}' -t 'All specs passed'"
-          end
+          #if failure_count > 0
+            #growlnotify "--image ./autotest/fail.png -p Emergency -m '#{summary}' -t 'Spec failure detected'"
+          #elsif pending_count > 0
+            #growlnotify "--image ./autotest/pending.png -p High -m '#{summary}' -t 'Pending spec(s) present'"
+          #else
+            #growlnotify "--image ./autotest/pass.png -p 'Very Low' -m '#{summary}' -t 'All specs passed'"
+          #end
         end
 
       private
